61fca598e341ba883373b3638e16ad3d6dc895c1
yangjingce_laptop <yangjingce@icloud.com>
2022-01-27 10:41:56 +0800

    add_multi_populations

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import geatpy as ea  # import geatpy
 from problem import MyProblem  # 导入自定义问题接口
 
-
 if __name__ == '__main__':
     """===============================实例化问题对象==========================="""
+    # PoolType = 'Thread'  # 设置采用多线程，若修改为: PoolType = 'Process'，则表示用多进程
     problem = MyProblem()  # 生成问题对象
     """=================================种群设置=============================="""
     Encoding = 'RI'  # 编码方式 实整数编码
+    # 单种群
     NIND = 1000  # 种群规模
     Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
     population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
+    NINDs = [250, 250, 250, 250]  # 种群规模
+    population = [None] * 4 # 创建种群列表
+    for i in range(len(NINDs)):
+        Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
+        population[i] = ea.Population(Encoding, Field, NINDs[i])  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
     """===============================算法参数设置============================="""
-    myAlgorithm = ea.soea_SEGA_templet(problem, population)  # 实例化一个算法模板对象
+    #myAlgorithm = ea.soea_SEGA_templet(problem, population)  # 实例化一个算法模板对象
+    myAlgorithm = ea.soea_multi_SEGA_templet(problem, population)  # 实例化一个算法模板对象
     myAlgorithm.MAXGEN = 100  # 最大进化代数
+    # myAlgorithm.recOper.XOVR = 0.7  # 交叉概率
+    # myAlgorithm.mutOper.Pm = 1  # 变异概率
     myAlgorithm.logTras = 1  # 设置每隔多少代记录日志，若设置成0则表示不记录日志
     myAlgorithm.verbose = True  # 设置是否打印输出日志信息
     myAlgorithm.drawing = 1  # 设置绘图方式（0：不绘图；1：绘制结果图；2：绘制目标空间过程动画；3：绘制决策空间过程动画）
@@ -25,7 +34,7 @@ if __name__ == '__main__':
     if BestIndi.sizes != 0:
         print('最优的目标函数值为：%s' % BestIndi.ObjV[0][0])
         print('最优的控制变量值为：')
-        for i in range(BestIndi.Phen.shape[1]):
-            print(BestIndi.Phen[0, i])
+        # for i in range(BestIndi.Phen.shape[1]):
+        #     print(BestIndi.Phen[0, i])
     else:
         print('没找到可行解。')
