5aac1ee91046c0e37d23cbd427f4a8b10387165b
yangjingce_laptop <yangjingce@icloud.com>
2022-01-27 13:04:02 +0800

    modify_multi_populations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import geatpy as ea  # import geatpy
+import numpy as np
+
 from problem import MyProblem  # 导入自定义问题接口
 
 if __name__ == '__main__':
@@ -8,20 +10,25 @@ if __name__ == '__main__':
     """=================================种群设置=============================="""
     Encoding = 'RI'  # 编码方式 实整数编码
     # 单种群
-    NIND = 1000  # 种群规模
-    Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
-    population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
-    NINDs = [250, 250, 250, 250]  # 种群规模
-    population = [None] * 4 # 创建种群列表
+    # NIND = 1000  # 种群规模
+    # Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
+    # population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
+    NIND = 1000  # 总种群规模
+    N_population = 4 # 种群数
+    NINDs = [NIND // N_population] * N_population  # 种群规模
+    population = [None] * N_population # 创建种群列表
     for i in range(len(NINDs)):
         Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
         population[i] = ea.Population(Encoding, Field, NINDs[i])  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
     """===============================算法参数设置============================="""
-    #myAlgorithm = ea.soea_SEGA_templet(problem, population)  # 实例化一个算法模板对象
-    myAlgorithm = ea.soea_multi_SEGA_templet(problem, population)  # 实例化一个算法模板对象
-    myAlgorithm.MAXGEN = 100  # 最大进化代数
+    # myAlgorithm = ea.soea_SEGA_templet(problem, population)  # 实例化一个算法模板对象
     # myAlgorithm.recOper.XOVR = 0.7  # 交叉概率
     # myAlgorithm.mutOper.Pm = 1  # 变异概率
+
+    myAlgorithm = ea.soea_multi_SEGA_templet(problem, population)  # 实例化一个算法模板对象
+    myAlgorithm.MAXGEN = 100  # 最大进化代数
+    myAlgorithm.trappedValue = 1e-6  # “进化停滞”判断阈值
+    myAlgorithm.maxTrappedCount = 100  # 进化停滞计数器最大上限值，如果连续maxTrappedCount代被判定进化陷入停滞，则终止进化
     myAlgorithm.logTras = 1  # 设置每隔多少代记录日志，若设置成0则表示不记录日志
     myAlgorithm.verbose = True  # 设置是否打印输出日志信息
     myAlgorithm.drawing = 1  # 设置绘图方式（0：不绘图；1：绘制结果图；2：绘制目标空间过程动画；3：绘制决策空间过程动画）
