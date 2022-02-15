aee52f026bd4105391d0a23f1ba7f79896e9879a
yangjingce_laptop <yangjingce@icloud.com>
2022-02-15 12:53:58 +0800

    modify_greedy_algorithm

--- referenceObjV/Greedy_algorithm_main.py
+++ referenceObjV/Greedy_algorithm_main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     num_cores = int(mp.cpu_count())  # 获得计算机的核心数
     pool = ProcessPool(num_cores)  # 设置池的大小
 
-    #初始解，所有缓存和计算在云端位置上
+    # 初始解，所有缓存和计算在云端位置上
     ans = np.zeros([2, model.N_device, model.N_task])
     # 初始化决策对象
     decision = Decision(model.N_cloud, model.N_FAP, model.N_user, model.N_task, model.device_cache, model.device_comput, model.task_cache, model.task_comput)
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     # 迭代次数
     N_loop = 1000
     # 算法开始
+    priority = np.arange(0, model.N_device, 1) # 优先级矩阵，排在前面的用户先做出卸载决策
     for loop in range(N_loop):
 
         # 计算当前的解的目标函数
@@ -30,8 +31,9 @@ if __name__ == '__main__':
         cur_objv = decision.get_average_user_delay()  # 目标函数值
         decision.calcul_cache_limit()  # 计算出缓存约束
         decision.calcul_comput_limit()  # 计算出计算约束
-        # 每个user依次决策
-        for user in range(model.N_cloud + model.N_FAP, model.N_device):
+        priority = np.argsort(decision.every_device_exp_delay)[0][::-1]
+        # 按照优先级矩阵，每个user依次决策
+        for user in priority:
             pass
 
         # 输出当前结果
