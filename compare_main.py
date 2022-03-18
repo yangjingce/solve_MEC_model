59b3c8f00b0eb0afe3d3f4188733a901ac48ff30
yangjingce <yangjingce@icloud.com>
2022-03-18 19:07:01 +0800

    修复比较代码的bug

--- compare_main.py
+++ compare_main.py
@@ -10,11 +10,18 @@ if __name__ == '__main__':
     o = np.zeros([N_test, 1])
 
     for i in range(N_test):
+        # 生成模型
         model = Model()
-        g_ans, g_cache, g_comput, g_arrive_loop = GreedyAlgorithm(model)
-        o_ans, o_cache, o_comput = OrderAlgorithm(model)
-        g[0, i] = g_ans
-        o[0, i] = o_ans
+        # 博弈算法
+        greedy = GreedyAlgorithm(model)
+        greedy.solve()
+        g_ans, g_cache, g_comput, g_arrive_loop = greedy.get_result()
+        # 遗传算法
+        order = OrderAlgorithm(model)
+        order.solve()
+        o_ans, o_cache, o_comput = order.get_result()
+        g[i, 0] = g_ans
+        o[i, 0] = o_ans
     t = np.hstack([g, o])
     pd.DataFrame(t).to_csv('compare_ans.csv')
 
