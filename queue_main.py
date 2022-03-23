499cee4d0d48ff2fb57f75dad9f133af55cb3a50
yangjingce <yangjingce@icloud.com>
2022-03-23 12:20:28 +0800

    遗传算法改回单种群优化

--- queue_main.py
+++ queue_main.py
@@ -39,22 +39,22 @@ class QueueAlgorithm:
         """=================================种群设置=============================="""
         Encoding = 'P'  # 编码方式 排列编码
         # 单种群
-        # NIND = 1000  # 种群规模
-        # Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
-        # population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
-        NIND = 20  # 总种群规模
-        N_population = 4  # 种群数
-        NINDs = [NIND // N_population] * N_population  # 种群规模
-        population = [None] * N_population  # 创建种群列表
+        NIND = 20  # 种群规模
         Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
-        for i in range(N_population):
-            population[i] = ea.Population(Encoding, Field, NINDs[i])  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
+        population = ea.Population(Encoding, Field, NIND)  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
+        # NIND = 20  # 总种群规模
+        # N_population = 4  # 种群数
+        # NINDs = [NIND // N_population] * N_population  # 种群规模
+        # population = [None] * N_population  # 创建种群列表
+        # Field = ea.crtfld(Encoding, problem.varTypes, problem.ranges, problem.borders)  # 创建区域描述器
+        # for i in range(N_population):
+        #     population[i] = ea.Population(Encoding, Field, NINDs[i])  # 实例化种群对象（此时种群还没被初始化，仅仅是完成种群对象的实例化）
         """===============================算法参数设置============================="""
         # myAlgorithm = ea.soea_SEGA_templet(problem, population)  # 实例化一个算法模板对象
         # myAlgorithm.recOper.XOVR = 0.7  # 交叉概率
         # myAlgorithm.mutOper.Pm = 1  # 变异概率
 
-        myAlgorithm = ea.soea_multi_SEGA_templet(problem, population)  # 实例化一个算法模板对象
+        myAlgorithm = ea.soea_SEGA_templet(problem, population)  # 实例化一个算法模板对象
         myAlgorithm.MAXGEN = 20  # 最大进化代数
         myAlgorithm.trappedValue = 1e-6  # “进化停滞”判断阈值
         myAlgorithm.maxTrappedCount = 3  # 进化停滞计数器最大上限值，如果连续maxTrappedCount代被判定进化陷入停滞，则终止进化
@@ -96,10 +96,9 @@ class QueueAlgorithm:
 
 
 if __name__ == '__main__':
-    for i in range(3):
-        print('-----------------', i, '-----------------')
-        t = QueueAlgorithm(Model())
-        t.solve()
-        t.print()
-        print(t.get_result()[0])
-        print(t.get_result()[0])
+
+    t = QueueAlgorithm(Model())
+    t.solve()
+    t.print()
+    print(t.get_result()[0])
+    print(t.get_result()[0])
